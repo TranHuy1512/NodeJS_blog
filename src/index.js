19b5be9b2a01aad1e1db784ba14d304ae326da76
Elnino <tdhuy1512@gmail.com>
2024-11-20 16:31:47 +0700

    test husky

--- src/index.js
+++ src/index.js
@@ -8,23 +8,30 @@ const port = 3000;
 const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 //HTTP logger
 // app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', engine({
-    extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 // console.log('PATH: ', path.join(__dirname,'resources/views'))
 
 //routes init
-route(          app);
+route(      app);
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`),
+);
